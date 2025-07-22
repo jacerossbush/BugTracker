a77a8f1121ccab5e3486e96f762cd6ef62fcdcb4
Jace Bush <jacerossbush@gmail.com>
2025-07-22 07:48:53 -0600

    Add in Database Connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,26 @@
+require('dotenv').config();
 const express = require('express');
-const port = 3000
+const { Pool } = require('pg');
+const port = 3000;
 
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
 const app = express();
 app.use(express.json());
+
+// Test the database connection
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack);
+    }
+    console.log('Connected to PostgreSQL database');
+    release(); // Release the client back to the pool
+});
 
 app.get('/', (req, res) => {
     res.send('express is working baby');
